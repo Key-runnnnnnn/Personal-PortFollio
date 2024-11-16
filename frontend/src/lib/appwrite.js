574feb5734b36e95c33f278aa3aade5524b56728
kiran <kiran.s.dhawan@gmail.com>
2024-11-16 15:22:36 +0530

    "Update Appwrite project ID in client configuration"

--- frontend/src/lib/appwrite.js
+++ frontend/src/lib/appwrite.js
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('66d8a1190014cb46b96b');
+  .setProject('66e54f2c0029782c61f9');
 
 export const account = new Account(client);
 export { ID } from 'appwrite'; 
